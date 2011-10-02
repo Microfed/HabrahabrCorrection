e52c8dd89623da90d9b60029eefc2c05a72bacee
Microfed <microfed@gmail.com>
2011-10-02 07:18:58 +0400

    Попытка облегчить вес расширнеия путем удаления jquery-библиотек из пакета провалилась  - пока не получается динамически подгружать скрипты внутри content script.
    + Добавлен скрипт для удобного билда решения.

---
+++ pack-cmd.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set root-dir=%~dp0
+set lib-utility-path=%root-dir%lib-utility\
+set yuicompressor-path=%lib-utility-path%yuicompressor-2.4.6.jar
+set pack-path=%root-dir%ready-to-pack\
+set pack-js-path=%pack-path%js
+set pack-css-path=%pack-path%css
+set pack-image-path=%pack-path%i
+set js-path=%root-dir%js
+set css-path=%root-dir%css
+set image-path=%root-dir%i
+
+SET COPYCMD=/Y
+
+echo "Copying scripts..."
+xcopy %js-path% %pack-js-path% /i /d /s
+
+echo "Copying css-styles..."
+xcopy %css-path% %pack-css-path% /i /d /s
+
+echo "Copying images..."
+xcopy %image-path% %pack-image-path% /i /d /s
+
+echo "Copying manifest..."
+xcopy %root-dir%manifest.json %pack-path%manifest.json /d
+
+echo "Copying html-pages..."
+xcopy %root-dir%background.html %pack-path%background.html /d
+xcopy %root-dir%options.html %pack-path%options.html /d
+
+echo "Compessing and moving a content script..."
+java -jar %yuicompressor-path% -o %pack-js-path%\content-script.js %js-path%\content-script.js
+
+
+
